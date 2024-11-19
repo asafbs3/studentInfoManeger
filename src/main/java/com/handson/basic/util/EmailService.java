80d85a2464e3de0674d03c4223ecee604b2773d6
asafbs3 <97667206+asafbs3@users.noreply.github.com>
2024-11-19 18:27:24 +0200

    Add files via upload

---
+++ src/main/java/com/handson/basic/util/EmailService.java
@@ -0,0 +1,25 @@
+package com.handson.basic.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendEmail(String toEmail,
+                          String subject,
+                          String body){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("asafbs3@gmail.com");
+        message.setTo(toEmail);
+        message.setSubject(subject);
+        message.setText(body);
+
+        mailSender.send(message);
+        System.out.println("Message sending");
+    }
+}
